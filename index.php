bcca3e7d3d3a85a56506da432126d68a4fd6a95e
robine_k <robine_k@etna-alternance.net>
2022-01-20 09:52:58 +0100

    modif title and desc - MAJ 1.3

--- index.php
+++ index.php
@@ -14,12 +14,14 @@
 
 <body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="77">
     <nav class="navbar navbar-light navbar-expand-md fixed-top" id="mainNav">
-        <div class="container"><a class="navbar-brand" href="#">Outils de prise de notes&nbsp;</a><button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
+        <div class="container"><a class="navbar-brand" href="#">Outils de presentation&nbsp;</a><button data-bs-toggle="collapse" class="navbar-toggler navbar-toggler-right" data-bs-target="#navbarResponsive" type="button" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" value="Menu"><i class="fa fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item nav-link"><a class="nav-link active" href="#createnote">creer une note</a></li>
                     <li class="nav-item nav-link"><a class="nav-link" href="#NOTE">dernières notes</a></li>
-                    <li class="nav-item nav-link"><a class="nav-link" href="#infoCollect">Collect_info</a></li>
+                    <li class="nav-item nav-link"><a class="nav-link" href="#infoCollect">Collecte d'info</a></li>
+                    <li class="nav-item nav-link"><a class="nav-link" href="#finalrapport">rapport Final</a></li>
+                  
                 </ul>
             </div>
         </div>
@@ -30,7 +32,7 @@
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
                         <h1 class="brand-heading">Bienvenue&nbsp;</h1>
-                        <p class="intro-text">Bienvenue sur l'outil de prise de note, cette outil permet une prise de notes rapides, il permet également de trier les différentes notes et intègre un module de collecte d’informations&nbsp; &nbsp;</p><a class="btn btn-link btn-circle" role="button" href="#about"><i class="fa fa-angle-double-down animated"></i></a>
+                        <p class="intro-text">Bienvenue sur l'outil de présentation de situiation Géopolitique, cette outil permet une prise de notes rapides, il permet également de trier les différentes notes et intègre un module de collecte d’informations MultiCanal(Twitter, Google, Flux RSS de journaux)&nbsp; &nbsp;</p><a class="btn btn-link btn-circle" role="button" href="#about"><i class="fa fa-angle-double-down animated"></i></a>
                     </div>
                 </div>
             </div>
